142e32c73d14604c0cd2e0628949318cd09c7ec0
mcryan2 <ryancmaya@gmail.com>
2018-08-14 20:10:46 +0000

    automatically backed up by learn

--- app/models/model.rb
+++ app/models/model.rb
@@ -1,3 +1,19 @@
-class Model
-  # Replace with Dog clas
+class Dog
+  attr_accessor :name, :breed, :age 
+                    # note: remember in burrito, our getter #setter 
+                    # method? attr_accessor takes care of both of those
+  DOGS = []
+  def initialize(name, breed, age)
+    @name = name
+    @breed = breed
+    @age = age
+    DOGS << self 
+                    # DOGS is just an array. self refers to all 
+                    # attributes to this class. Push in all of the above # into this array to return it later. Advanced 
+                    # poop. 
 end
+
+def self.assert_select_all 
+  DOGS 
+end
+end  
